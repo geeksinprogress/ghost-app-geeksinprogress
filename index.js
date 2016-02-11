2396d8b96a68fe61e27ea401d858e8c36a927c2b
Michael Chinn <git@mechinn.com>
2016-02-11 01:38:17 -0500

    basic Ghost App

---
+++ index.js
@@ -0,0 +1,10 @@
+var GhostApp = require('ghost-app');
+
+var JekyllMd = GhostApp.extend({
+    install: function () {},
+    uninstall: function () {},
+    activate: function () {},
+    deactivate: function () {}
+});
+
+module.exports = JekyllMd;
