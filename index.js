b27b66ad18ca6f4c2ddd9669f353d9b991be9ae2
Michael Chinn <git@mechinn.com>
2016-02-13 01:25:34 -0500

    got yaml front matter working similar to how jekyll works but with +++ instead of ---, this will alow us to define variables and whatnot to influence how the template is generated, also added helpers for logic blocks in handlebars and for now the rss feed will just remove the front matter and not do anything with it

--- index.js
+++ index.js
@@ -1,10 +1,114 @@
-var GhostApp = require('ghost-app');
+var GhostApp = require('ghost-app'),
+    Showdown = require('showdown-ghost'),
+    converter = new Showdown.converter({extensions: ['ghostgfm', 'footnotes', 'highlight']}),
+    frontMatter = require('gray-matter');
 
-var JekyllMd = GhostApp.extend({
+var delimeter = '+++';
+var frontMatterOptions = {
+    delims: [delimeter,delimeter]
+};
+
+var parse = function(post) {
+	var extract = frontMatter(post.markdown, frontMatterOptions);
+    post.markdown = extract.content;
+    post.html = converter.makeHtml(post.markdown);
+    post.data = extract.data;
+};
+
+var getData = function(text) {
+	return frontMatter(text, frontMatterOptions).data;
+};
+
+var ignore = function(text) {
+	var regexDelimeter = delimeter.replace(/\+/g,'\\+');
+	var regexString = '<p>'+regexDelimeter+'[\\S\\s]*'+regexDelimeter+'<\\/p>';
+	console.log(regexString);
+	var finder = new RegExp(regexString,'m');
+	return text.replace(finder,"");
+};
+
+var GeeksInProgress = GhostApp.extend({
+	filters: {
+       prePostsRender: [5, 'postFrontMatter'],
+       "rss.item": [5, 'rssItemFrontMatter'],
+       "rss.feed": [5, 'rssFeedFrontMatter']
+    },
+    postFrontMatter: function (posts) {
+        if (posts instanceof Array) {
+            posts.forEach(function(post) {
+                parse(post);
+            });
+        } else {
+            parse(posts);
+        }
+        return posts;
+    },
+    rssItemFrontMatter: function(item, post) {
+		var data = getData(post.markdown);
+
+		//cleanup front matter from description and content
+		var contentEncoded = item.custom_elements[0]['content:encoded'];
+		contentEncoded._cdata = ignore(contentEncoded._cdata);
+		item.description = ignore(item.description);
+        return item;
+    },
+    rssFeedFrontMatter: function(feed) {
+
+        return feed;
+    },
     install: function () {},
     uninstall: function () {},
-    activate: function () {},
+    activate: function () {
+    	this.ghost.helpers.register('cond', function (v1, operator, v2, options) {
+	        switch (operator) {
+	            case '==':
+	                return (v1 == v2) ? options.fn(this) : options.inverse(this);
+	            case '!=':
+	                return (v1 == v2) ? options.fn(this) : options.inverse(this);
+	            case '===':
+	                return (v1 === v2) ? options.fn(this) : options.inverse(this);
+	            case '<':
+	                reaturn (v1 < v2) ? options.fn(this) : options.inverse(this);
+	            case '<=':
+	                return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+	            case '>':
+	                return (v1 > v2) ? options.fn(this) : options.inverse(this);
+	            case '>=':
+	                return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+	            case '&&':
+	                return (v1 && v2) ? options.fn(this) : options.inverse(this);
+	            case '||':
+	                return (v1 || v2) ? options.fn(this) : options.inverse(this);
+	            default:
+	                return options.inverse(this);
+	        }
+	    });
+	    this.ghost.helpers.register('eq', function (v1, v2, options) {
+	        return (v1 == v2) ? options.fn(this) : options.inverse(this);
+	    });
+	    this.ghost.helpers.register('ne', function (v1, v2, options) {
+	        return (v1 != v2) ? options.fn(this) : options.inverse(this);
+	    });
+	    this.ghost.helpers.register('lt', function (v1, v2, options) {
+	        return (v1 < v2) ? options.fn(this) : options.inverse(this);
+	    });
+	    this.ghost.helpers.register('le', function (v1, v2, options) {
+	        return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+	    });
+	    this.ghost.helpers.register('gt', function (v1, v2, options) {
+	        return (v1 > v2) ? options.fn(this) : options.inverse(this);
+	    });
+	    this.ghost.helpers.register('ge', function (v1, v2, options) {
+	        return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+	    });
+	    this.ghost.helpers.register('and', function (v1, v2, options) {
+	        return (v1 && v2) ? options.fn(this) : options.inverse(this);
+	    });
+	    this.ghost.helpers.register('or', function (v1, v2, options) {
+	        return (v1 || v2) ? options.fn(this) : options.inverse(this);
+	    });
+    },
     deactivate: function () {}
 });
 
-module.exports = JekyllMd;
+module.exports = GeeksInProgress;
