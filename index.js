6f2ebe1f972657d126e83405244c5e2fd139ee04
Michael Chinn <git@mechinn.com>
2016-02-14 00:54:55 -0500

    ok i think we have a working podcast xml and yaml metadata within an html comment now instead of +++

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var GhostApp = require('ghost-app'),
+	downsize = require('downsize'),
+	Promise = require('bluebird'),
     Showdown = require('showdown-ghost'),
     converter = new Showdown.converter({extensions: ['ghostgfm', 'footnotes', 'highlight']}),
     frontMatter = require('gray-matter');
 
-var delimeter = '+++';
 var frontMatterOptions = {
-    delims: [delimeter,delimeter]
+    delims: ['<!---','--->']
 };
 
 var parse = function(post) {
@@ -20,11 +21,7 @@ var getData = function(text) {
 };
 
 var ignore = function(text) {
-	var regexDelimeter = delimeter.replace(/\+/g,'\\+');
-	var regexString = '<p>'+regexDelimeter+'[\\S\\s]*'+regexDelimeter+'<\\/p>';
-	console.log(regexString);
-	var finder = new RegExp(regexString,'m');
-	return text.replace(finder,"");
+	return text.replace(/^<!---[\S\s]*--->/m, "");
 };
 
 var GeeksInProgress = GhostApp.extend({
@@ -44,21 +41,76 @@ var GeeksInProgress = GhostApp.extend({
         return posts;
     },
     rssItemFrontMatter: function(item, post) {
+    	console.log(post);
 		var data = getData(post.markdown);
-
-		//cleanup front matter from description and content
-		var contentEncoded = item.custom_elements[0]['content:encoded'];
-		contentEncoded._cdata = ignore(contentEncoded._cdata);
+		console.log(data);
+		item.custom_elements[0]['content:encoded']._cdata = ignore(item.custom_elements[0]['content:encoded']._cdata);
 		item.description = ignore(item.description);
+		if(data.type == 'podcast') {
+			if(data.guid) {
+				item.guid = data.guid;
+			}
+			item.enclosure = {
+			  'url'  : data.file,
+			  'size' : data.size,
+			  'type' : 'audio/mpeg'
+			}
+			item.custom_elements = [
+				{'itunes:subtitle': post.meta_description || downsize(ignore(post.html), {words: 15})},
+				{'itunes:duration': data.duration},
+				{'itunes:explicit': data.explicit || 'yes'}
+			];
+			if(post.image) {
+				item.custom_elements.append({'itunes:image': {_attr: {href: post.image}}});
+			}
+		}
         return item;
     },
     rssFeedFrontMatter: function(feed) {
-
+		var siteData = {};
+		feed.copyright = '© & Ⓟ '+new Date().getFullYear()+' '+feed.title;
+    	feed.language = 'en-us';
+    	feed.image_url = feed.site_url+'files/logo.jpg';
+    	var feedParts = feed.feed_url.split("/");
+    	var secure = feedParts[0] == 'https';
+    	var domain = feedParts[2];
+    	var path = feedParts[3];
+    	if('tag' == path) {
+    		var type = feedParts[4];
+    		if('podcast' == type) {
+    			feed.pubDate = new Date().toUTCString();
+	    		feed.custom_namespaces = {
+	    			itunes: "http://www.itunes.com/dtds/podcast-1.0.dtd"
+	    		};
+				feed.custom_elements = [
+					{'itunes:subtitle': 'Listen in on the geeks in progress'},
+					{'itunes:author': feed.title},
+					{'itunes:summary': feed.description},
+					{'itunes:owner': [
+						{'itunes:name': 'Michael Chinn'},
+						{'itunes:email': 'apple@mechinn.com'}
+					]},
+					{'itunes:image': {_attr: {href: feed.image_url}}},
+					{'itunes:category': [
+						{_attr: {text: 'Games & Hobbies'}},
+						{'itunes:category': {_attr: {text: 'Video Games'}}}
+					]},
+					{'itunes:category': {_attr: {text: 'TV & Film'}}},
+					{'itunes:category': [
+						{_attr: { text: 'Games & Hobbies' }},
+						{'itunes:category': { _attr: { text: 'Hobbies' } }}
+					]},
+					{'itunes:explicit': 'yes'}
+				];
+	    	} 
+    	}
+    	console.log(feed);
         return feed;
     },
     install: function () {},
     uninstall: function () {},
     activate: function () {
+    	console.log(this);
     	this.ghost.helpers.register('cond', function (v1, operator, v2, options) {
 	        switch (operator) {
 	            case '==':
